3b30958de15c0976fc8ba1207cea8ba17b5f759c
Yuchan Han <serltretu24@gmail.com>
2024-06-16 10:43:07 +0900

    rename main file

--- main.py
+++ main.py
@@ -2,26 +2,28 @@ import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
 
+
 def visualize_trend_with_prediction(region, period, price, future_price):
-    years = list(range(-period+1, 1))  # 과거 기간 리스트
-    
+    years = list(range(-period + 1, 1))  # 과거 기간 리스트
+
     # 그래프 설정
     plt.figure(figsize=(10, 6))
-    plt.plot(years, price, marker='o', linestyle='-', label='Current Price')
-    plt.title(f'{region} House Price')
-    plt.xlabel('Year')
-    plt.ylabel('Price (in 만원)')
+    plt.plot(years, price, marker="o", linestyle="-", label="Current Price")
+    plt.title(f"{region} House Price")
+    plt.xlabel("Year")
+    plt.ylabel("Price (in 만원)")
     plt.grid(True)
 
     # 예측된 가격
     future_year = 1
-    plt.scatter(future_year, future_price, color='red', label='Future Price')
-    
+    plt.scatter(future_year, future_price, color="red", label="Future Price")
+
     plt.legend()
     plt.show()
 
+
 def predict_future_price(region, period, price):
-    years = list(range(-period+1, 1))  # 과거 기간 리스트 생성
+    years = list(range(-period + 1, 1))  # 과거 기간 리스트 생성
     X = torch.tensor(years, dtype=torch.float32).view(-1, 1)  # 년도를 텐서로 변환
     y = torch.tensor(price, dtype=torch.float32).view(-1, 1)  # 집값을 텐서로 변환
 
@@ -43,12 +45,13 @@ def predict_future_price(region, period, price):
         optimizer.step()  # 옵티마이저 스텝 진행
 
         if epoch % 1000 == 0:
-            print(f'Epoch [{epoch}/{num_epochs}], Loss: {loss.item():.4f}')
+            print(f"Epoch [{epoch}/{num_epochs}], Loss: {loss.item():.4f}")
 
     model.eval()  # 평가 모드로 설정
     future_year = torch.tensor([[1]], dtype=torch.float32)  # 미래 년도 설정
     future_price = model(future_year).item()  # 미래 가격 예측
     return future_price
+
 
 region = input("지역을 입력하세요: ")
 period = int(input("과거 몇년간의 기간을 입력하시겠습니까?: "))
